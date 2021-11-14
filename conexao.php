ea5fa57079bc102f0ba4c3c35e09eadc6e042977
Luis Henrique da Silva Campos <64844248+Flameuss@users.noreply.github.com>
2021-11-13 22:20:53 -0300

    Conexão bd
    Forma de connectar ao bd

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST','127.0.0.1');
-define('USUARIO','root');
-define('SENHA','Di140617');
-define('DB','devtop');
+define('HOST','127.0.0.1'); // aqui você insere o caminho do seu host local, por padrão  "127.0.0.1"
+define('USUARIO','root'); // aqui o usuário de conexão ao banco de dados, por padrão do xampp "root"
+define('SENHA',''); //aqui vai a senha do seu banco de dados
+define('DB',''); // aqui o nome do banco de dados
 //Criar a conexao
 $conn = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel connectar!');
 ?>
